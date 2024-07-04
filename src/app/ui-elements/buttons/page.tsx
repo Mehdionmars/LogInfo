52d665e2ed72f2f0e129370e0c828d2c7f5ff759
Mehdi <106988771+Mehdionmars@users.noreply.github.com>
2024-07-04 23:40:09 +0200

    Update page.tsx

--- src/app/ui-elements/buttons/page.tsx
+++ src/app/ui-elements/buttons/page.tsx
@@ -1,1 +1,12 @@
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLaout";
+import ButtonDefault from "@/components/Buttons/ButtonDefault";
 
+const Buttons = () => {
+  return (
+
+  );
+};
+
+export default Buttons;
