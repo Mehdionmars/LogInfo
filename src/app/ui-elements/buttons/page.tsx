44cd6c6b892cdf07cf732763e2ddac4e86d06787
Mehdi <106988771+Mehdionmars@users.noreply.github.com>
2024-07-05 00:18:53 +0200

    Update page.tsx

--- src/app/ui-elements/buttons/page.tsx
+++ src/app/ui-elements/buttons/page.tsx
@@ -4,9 +4,6 @@ import DefaultLayout from "@/components/Layouts/DefaultLaout";
 import ButtonDefault from "@/components/Buttons/ButtonDefault";
 
 const Buttons = () => {
-  return (
-
-  );
 };
 
 export default Buttons;
